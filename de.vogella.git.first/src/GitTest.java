5b9aa85def09959c8af47287138d6f3f1df02491
weblessthelord <sophymabena@webmail.co.za>
2012-06-26 17:01:39 +0300

    let us see update in eclipse

--- de.vogella.git.first/src/GitTest.java
+++ de.vogella.git.first/src/GitTest.java
@@ -1,7 +1,7 @@
-
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
 	//commit to the new repository
+	//let us see if it is updated in eclipse
 	}
 }
